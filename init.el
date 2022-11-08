9ada8f54ecf4af49711e9be73a2b9e83332c2c3a
Tibor Pilz <tibor.pilz@avenga.com>
2022-11-08 20:13:07 +0100

    fix ivy

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
        (company           ; the ultimate code completion backend
         +childframe)
        ;;ido               ; the other *other* search engine...
-       (ivy +childframe +fuzzy +icons +prescient)               ; a search engine for love and lifoe
+       (ivy +childframe +fuzzy +icons)               ; a search engine for love and lifoe
        ;; (helm +fuzzy)      ; the *other* search engine for love and life
        ;; (vertico +icons +childframe)           ; the search engine of the future
 
@@ -58,7 +58,7 @@
        (evil +everywhere); come to the dark side, we have cookies
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
-       (format +onsave)  ; automated prettiness
+       ;; (format +onsave)  ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
        ;;multiple-cursors  ; editing in many places at once
@@ -90,7 +90,7 @@
        :tools
        ansible
        biblio            ; Writes a PhD for you (citation needed)
-       (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
+       ;; (debugger +lsp)          ; FIXME stepping through code, to help you add bugs
        direnv
        docker
                                         ; editorconfig      ; let someone else argue about tabs vs spaces
@@ -143,7 +143,7 @@
        ;;idris             ; a language you can depend on
        json              ; At least it ain't XML
        ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
-       (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
+       (javascript)
        (julia +lsp)             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
        (latex             ; writing papers in Emacs has never been so fun
