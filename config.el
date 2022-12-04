267274696226a0f15db633dda23b8d98af6ac0fc
Tibor Pilz <tibor@pilz.berlin>
2022-12-04 05:00:41 +0100

    org: babel tangle on save

--- config.el
+++ config.el
@@ -1,6 +1,9 @@
+;; [[file:config.org::*Personal data][Personal data:1]]
 (setq user-full-name "Tibor Pilz"
       user-mail-address "tibor@pilz.berlin")
+;; Personal data:1 ends here
 
+;; [[file:config.org::*Add checks for different machines][Add checks for different machines:1]]
 (defun is-mac ()
   (string-equal system-type "darwin"))
 
@@ -9,22 +12,32 @@
 
 (defun is-workstation ()
   (string-equal (system-name) "archyMcArchstation"))
+;; Add checks for different machines:1 ends here
 
+;; [[file:config.org::*Font selection][Font selection:1]]
 (setq font-scale-factor (if (is-workstation) 1.1 1.0))
 
 (defun scale-font (size)
   (round (* size font-scale-factor)))
+;; Font selection:1 ends here
 
+;; [[file:config.org::*Font selection][Font selection:2]]
 (when (is-mac) (setq doom-font (font-spec :family "FiraCode Nerd Font" :size (scale-font 14))) nil)
 ;; (setq doom-font (font-spec :family "FiraCode Nerd Font" :size (scale-font 16) :weight 'light)
 ;;       doom-big-font (font-spec :family "FiraCode Nerd Font" :size (scale-font 24))
 ;;       doom-variable-pitch-font (font-spec :family "Open Sans" :size (scale-font 16))
 ;;       doom-serif-font (font-spec :family "FreeSerif" :weight 'light))
+;; Font selection:2 ends here
 
+;; [[file:config.org::*Line numbers][Line numbers:1]]
 (setq display-line-numbers-type 'relative)
+;; Line numbers:1 ends here
 
+;; [[file:config.org::*Tab width][Tab width:1]]
 (setq tab-width 2)
+;; Tab width:1 ends here
 
+;; [[file:config.org::*Base Settings][Base Settings:1]]
 (setq org-directory "~/org/")
 (setq org-agenda-files (list org-directory))
 
@@ -37,23 +50,33 @@
 (setq org-return-follows-link 1)
 (setq calendar-week-start-day 1) ;; start on monday
 (setq org-agenda-include-diary t)
+;; Base Settings:1 ends here
 
+;; [[file:config.org::*mixed pitch & org-pretty-mode][mixed pitch & org-pretty-mode:1]]
 (add-hook 'org-mode-hook #'+org-pretty-mode)
+;; mixed pitch & org-pretty-mode:1 ends here
 
+;; [[file:config.org::*Show passed deadlines as error][Show passed deadlines as error:1]]
 (setq org-agenda-deadline-faces
       '((1.001 . error)
         (1.0 . org-warning)
         (0.5 . org-upcoming-deadline)
         (0.0 . org-upcoming-distant-deadline)))
+;; Show passed deadlines as error:1 ends here
 
+;; [[file:config.org::*Show quote blocks in italic][Show quote blocks in italic:1]]
 (setq org-fontify-quote-and-verse-blocks t)
+;; Show quote blocks in italic:1 ends here
 
+;; [[file:config.org::*Defer font-lock][Defer font-lock:1]]
 (defun locally-defer-font-lock ()
   "Set jit-lock defer and stealth, when buffer is over a certain size."
   (when (> (buffer-size) 50000)
     (setq-local jit-lock-defer-time 0.05
                 jit-lock-stealth-time 1)))
+;; Defer font-lock:1 ends here
 
+;; [[file:config.org::*Symbols][Symbols:1]]
 (after! org-superstar
   (setq org-superstar-headline-bullets-list '("◉" "○" "✸" "✿" "✤" "✜" "◆" "▶")
         org-superstar-prettify-item-bullets t ))
@@ -68,10 +91,14 @@
         (?C . 'all-the-icons-yellow)
         (?D . 'all-the-icons-green)
          (?E . 'all-the-icons-blue)))
+;; Symbols:1 ends here
 
+;; [[file:config.org::*HTTP requests via babel][HTTP requests via babel:1]]
 (use-package! ob-http
   :commands org-babel-execute:http)
+;; HTTP requests via babel:1 ends here
 
+;; [[file:config.org::*Babel header args][Babel header args:1]]
 (setq org-babel-default-header-args
       '((:session . "none")
         (:results . "replace")
@@ -81,7 +108,9 @@
         (:hlines . "no")
         (:tangle . "no")
         (:comments . "link")))
+;; Babel header args:1 ends here
 
+;; [[file:config.org::*LSP in org-babel src blocks][LSP in org-babel src blocks:1]]
 (cl-defmacro lsp-org-babel-enable (lang)
   "Support LANG in org source code block."
   (setq centaur-lsp 'lsp-mode)
@@ -110,7 +139,21 @@
   '("go" "python" "ipython" "bash" "sh" "ditaa"))
 (dolist (lang org-babel-lang-list)
   (eval `(lsp-org-babel-enable ,lang)))
+;; LSP in org-babel src blocks:1 ends here
 
+;; [[file:config.org::*Auto-Tanglins][Auto-Tanglins:1]]
+(defun org-babel-tangle-config ()
+  (when (string-equal (file-name-non-directory (buffer-file-name))
+                      "config.org")
+    (let ((org-config-babel-evaluate nil))
+      (org-babel-tangle))))
+
+(add-hook 'org-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook #'org-babel-tangle-config)))
+;; Auto-Tanglins:1 ends here
+
+;; [[file:config.org::*View exported file][View exported file:1]]
 (map! :map org-mode-map
       :localleader
       :desc "View exported file" "v" #'org-view-output-file)
@@ -138,23 +181,39 @@
   "Search for output files with these extensions, in order, viewing the first that matches")
 (defvar org-view-external-file-extensions '("html")
   "File formats that should be opened externally.")
+;; View exported file:1 ends here
 
+;; [[file:config.org::*use github markdown][use github markdown:1]]
 (use-package! ox-gfm :after ox)
+;; use github markdown:1 ends here
 
+;; [[file:config.org::*Export headings up to five levels deep][Export headings up to five levels deep:1]]
 (setq org-export-headline-levels 5)
+;; Export headings up to five levels deep:1 ends here
 
+;; [[file:config.org::*Ignore tag][Ignore tag:1]]
 ;(require 'ox-extra)
 ;(ox-extras-activate '(ignore-headlines))
+;; Ignore tag:1 ends here
 
+;; [[file:config.org::*automatic latex rendering][automatic latex rendering:1]]
 (use-package! org-fragtog
   :hook (org-mode . org-fragtog-mode))
+;; automatic latex rendering:1 ends here
 
+;; [[file:config.org::*Latex fragments][Latex fragments:1]]
 (setq org-highlight-latex-and-related '(native script entities))
+;; Latex fragments:1 ends here
 
+;; [[file:config.org::*Export to Reveal.js][Export to Reveal.js:1]]
 ;(use-package! org-re-reveal)
+;; Export to Reveal.js:1 ends here
 
+;; [[file:config.org::*Use the same directory as org][Use the same directory as org:1]]
 (setq org-roam-directory "~/org")
+;; Use the same directory as org:1 ends here
 
+;; [[file:config.org::*Add Org-Roam UI][Add Org-Roam UI:2]]
 (use-package! websocket
   :after org-roam)
 
@@ -171,19 +230,27 @@
     (browse-url-xdg-open (format "http://localhost:%d" org-roam-ui-port))))
 
 (setq org-roam-ui-open-on-start nil)
+;; Add Org-Roam UI:2 ends here
 
+;; [[file:config.org::*Google Calendar integration][Google Calendar integration:2]]
 ;; (use-package! org-gcal
 ;;   :config
 ;;   (setq org-gcal-client-id "CLIENT_ID"
 ;;         org-gcal-client-secret "CLIENT_SECRET"
 ;;         org-gcal-fetch-file-alit '(("tbrpilz@googlemail.com" . "~/org/schedule.org"))))
+;; Google Calendar integration:2 ends here
 
+;; [[file:config.org::*Visual-line-mode messes with with plaintext (markdow, latex)][Visual-line-mode messes with with plaintext (markdow, latex):1]]
 (remove-hook 'text-mode-hook #'visual-line-mode)
 (add-hook 'text-mode-hook #'auto-fill-mode)
+;; Visual-line-mode messes with with plaintext (markdow, latex):1 ends here
 
+;; [[file:config.org::*Prevent org-block face for latex fragments, since they look weird][Prevent org-block face for latex fragments, since they look weird:1]]
 (require 'org-src)
 (add-to-list 'org-src-block-faces '("latex" (:inherit default :extend t)))
+;; Prevent org-block face for latex fragments, since they look weird:1 ends here
 
+;; [[file:config.org::*Function to create an org buffer][Function to create an org buffer:1]]
 (evil-define-command evil-buffer-org-new (count file)
   "creates a new ORG buffer replacing the current window, optionally
    editing a certain FILE"
@@ -198,15 +265,21 @@
 (map! :leader
       (:prefix "b"
        :desc "new empty ORG buffer" "o" #'evil-buffer-org-new))
+;; Function to create an org buffer:1 ends here
 
+;; [[file:config.org::*Insert cdlatex enviornments and edit immediately][Insert cdlatex enviornments and edit immediately:1]]
 (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
 
 (defadvice! org-edit-latex-env-after-insert ()
   :after #'org-cdlatex-environment-indent
   (org-edit-latex-environment))
+;; Insert cdlatex enviornments and edit immediately:1 ends here
 
+;; [[file:config.org::*Disable auto-fill-mode][Disable auto-fill-mode:1]]
 (add-hook! markdown-mode (auto-fill-mode -1))
+;; Disable auto-fill-mode:1 ends here
 
+;; [[file:config.org::*Disable auto-fill-mode][Disable auto-fill-mode:2]]
 (use-package! org-appear
   :hook (org-mode . org-appear-mode)
   :config
@@ -216,62 +289,98 @@
   ;; for proper first-time setup, `org-appear--set-elements'
   ;; needs to be run after other hooks have acted.
   (run-at-time nil nil #'org-appear--set-elements))
+;; Disable auto-fill-mode:2 ends here
 
+;; [[file:config.org::*Project Search Path][Project Search Path:1]]
 (setq projectile-project-search-path '(("~/Code/" . 1)))
+;; Project Search Path:1 ends here
 
+;; [[file:config.org::*Jest Test Mode][Jest Test Mode:2]]
 (use-package! jest-test-mode
   :commands jest-test-mode
   :hook (typescript-mode js-mode typescript-tsx-mode))
+;; Jest Test Mode:2 ends here
 
+;; [[file:config.org::*Svelte][Svelte:2]]
 (use-package! svelte-mode
     :mode "\\.svelte\\'")
+;; Svelte:2 ends here
 
+;; [[file:config.org::*Formatting][Formatting:1]]
 (with-eval-after-load 'web-mode
   (setq web-mode-script-padding 0))
+;; Formatting:1 ends here
 
+;; [[file:config.org::*Tailwind][Tailwind:2]]
 (use-package! lsp-tailwindcss
   :init
   (setq lsp-tailwindcss-add-on-mode t))
+;; Tailwind:2 ends here
 
+;; [[file:config.org::*Code formatting][Code formatting:1]]
 (setq typescript-indent-level 2)
+;; Code formatting:1 ends here
 
+;; [[file:config.org::*nix-mode][nix-mode:1]]
 (use-package! nix-mode
   :mode "\\.nix\\'")
+;; nix-mode:1 ends here
 
+;; [[file:config.org::*Run pytest in virtualenv][Run pytest in virtualenv:1]]
 (add-hook! python-mode
   (advice-add 'python-pytest-file :before
               (lambda (&rest args)
                 (setq-local python-pytest-executable
                             (executable-find "pytest")))))
+;; Run pytest in virtualenv:1 ends here
 
+;; [[file:config.org::*Code blocks][Code blocks:1]]
 (use-package! polymode)
 (use-package! poly-markdown)
+;; Code blocks:1 ends here
 
+;; [[file:config.org::*Handling][Handling:1]]
 (setq company-idle-delay
       (lambda () (if (company-in-string-or-comment) nil 0.01)))
+;; Handling:1 ends here
 
+;; [[file:config.org::*Handling][Handling:2]]
 (setq company-minimum-prefix-length 1)
+;; Handling:2 ends here
 
+;; [[file:config.org::*Handling][Handling:3]]
 (setq company-selection-wrap-around t)
+;; Handling:3 ends here
 
+;; [[file:config.org::*Backends][Backends:1]]
 (setq company-backends '((company-capf company-dabbrev-code)))
+;; Backends:1 ends here
 
+;; [[file:config.org::*Looks][Looks:1]]
 (setq company-format-margin-function #'company-vscode-dark-icons-margin)
+;; Looks:1 ends here
 
+;; [[file:config.org::*Load via nix somehow][Load via nix somehow:2]]
 (setq copilot-node-executable
       (replace-regexp-in-string "\n" "" (shell-command-to-string ". $XDG_CONFIG_HOME/zsh/.zshrc; nvm which 16")))
 
 (use-package! copilot
   :bind (("<backtab>" . 'copilot-accept-completion)))
+;; Load via nix somehow:2 ends here
 
+;; [[file:config.org::*Python][Python:1]]
 (setq dap-python-debugger 'debugpy)
+;; Python:1 ends here
 
+;; [[file:config.org::*Fix Doom "+debugger/start"][Fix Doom "+debugger/start":1]]
 ;;;###autoload
 (defun +debugger/clear ()
   "Clear the debugger configuration from the doom-store."
   (interactive)
   (doom-store-rem (doom-project-root) "+debugger"))
+;; Fix Doom "+debugger/start":1 ends here
 
+;; [[file:config.org::*Fix Doom "+debugger/start"][Fix Doom "+debugger/start":2]]
 (setq debugger-start-copy (symbol-function '+debugger/start))
 
 ;;;###autoload
@@ -279,7 +388,9 @@
   "Start the debugger."
   (interactive)
   (funcall debugger-start-copy arg))
+;; Fix Doom "+debugger/start":2 ends here
 
+;; [[file:config.org::*Fix Doom "+debugger/start"][Fix Doom "+debugger/start":3]]
 ;;;###autoload
 (defun +debugger/start (arg)
   "Launch a debugger session.
@@ -289,19 +400,25 @@ for what debugger to use. If the prefix ARG is set, prompt anyway."
   (message arg)
   (+debugger--set-config (+debugger-completing-read))
   (+debugger/start-last))
+;; Fix Doom "+debugger/start":3 ends here
 
+;; [[file:config.org::*Get the window containing a file buffer][Get the window containing a file buffer:1]]
 (defun get-window-with-file-buffer ()
   "Get the window with a file buffer."
   (seq-find (lambda (window)
               (buffer-file-name (window-buffer window)))
             (window-list)))
+;; Get the window containing a file buffer:1 ends here
 
+;; [[file:config.org::*Reset file buffer window][Reset file buffer window:1]]
 (defun reset-file-window-buffer ()
   "Reset the file window's buffer."
   (let ((window (get-window-with-file-buffer)))
     (when window
       (set-window-buffer window (window-buffer window)))))
+;; Reset file buffer window:1 ends here
 
+;; [[file:config.org::*Add reset to window configuration change hook][Add reset to window configuration change hook:1]]
 (defun add-reset-file-window-buffer-hook (&rest args)
   "Add the reset-file-window-buffer function to the window-configuration-change-hook."
   (add-hook 'window-configuration-change-hook 'reset-file-window-buffer))
@@ -311,7 +428,9 @@ for what debugger to use. If the prefix ARG is set, prompt anyway."
     (remove-hook 'window-configuration-change-hook 'reset-file-window-buffer))
 
 (add-hook 'dap-mode-hook 'add-reset-file-window-buffer-hook)
+;; Add reset to window configuration change hook:1 ends here
 
+;; [[file:config.org::*Keybindings][Keybindings:1]]
 (map! :leader
       (:prefix-map ("d" . "debugger")
        :desc "Debug" "d" #'dap-debug
@@ -330,58 +449,100 @@ for what debugger to use. If the prefix ARG is set, prompt anyway."
         :desc "Set condition" "c" #'dap-breakpoint-condition
         :desc "Set log message" "m" #'dap-breakpoint-log-message
         :desc "Set hit condition" "h" #'dap-breakpoint-hit-condition)))
+;; Keybindings:1 ends here
 
+;; [[file:config.org::*Syntax Checking][Syntax Checking:1]]
 (setq flycheck-syntax-automatically '(save-mode-enable))
+;; Syntax Checking:1 ends here
 
+;; [[file:config.org::*Performance][Performance:1]]
+(setq lsp-use-lists)
+;; Performance:1 ends here
+
+;; [[file:config.org::*Handling][Handling:1]]
 (setq lsp-completion-provider :capf)
+;; Handling:1 ends here
 
+;; [[file:config.org::*Handling][Handling:2]]
 (setq lsp-completion-show-detail nil)
+;; Handling:2 ends here
 
+;; [[file:config.org::*Handling][Handling:3]]
 (setq lsp-completion-show-kind t)
+;; Handling:3 ends here
 
+;; [[file:config.org::*UI][UI:1]]
 (map! :leader
       (:prefix ("c" . "code")
        :desc "Glance at documentation" "g" #'lsp-ui-doc-glance))
+;; UI:1 ends here
 
+;; [[file:config.org::*UI][UI:2]]
 (setq lsp-lens-enable t)
+;; UI:2 ends here
 
+;; [[file:config.org::*UI][UI:3]]
 (setq lsp-headerline-breadcrub-enable t)
+;; UI:3 ends here
 
+;; [[file:config.org::*UI][UI:4]]
 (setq lsp-eldock-enable-hover nil)
+;; UI:4 ends here
 
+;; [[file:config.org::*UI][UI:5]]
 (setq lsp-signature-auto-activate nil)
 (setq lsp-signature-render-documentation nil)
+;; UI:5 ends here
 
+;; [[file:config.org::*Doom Themes][Doom Themes:2]]
 (setq doom-theme 'doom-nord-aurora)
+;; Doom Themes:2 ends here
 
+;; [[file:config.org::*Nano][Nano:1]]
 ;; (add-to-list 'load-path "~/Code/doom-nano-testing")
 ;; (require 'load-nano)
 ;; (setq doom-themes-treemacs-theme "doom-atom")
+;; Nano:1 ends here
 
+;; [[file:config.org::*Nano Modeline][Nano Modeline:2]]
 ;; (use-package! nano-modeline
 ;;   :config
 ;;   (nano-modeline-mode 1))
+;; Nano Modeline:2 ends here
 
+;; [[file:config.org::*Doom Modeline][Doom Modeline:1]]
 (setq doom-modeline-vcs-max-length 50)
+;; Doom Modeline:1 ends here
 
+;; [[file:config.org::*Doom Modeline][Doom Modeline:2]]
 (setq doom-modeline-hud t)
+;; Doom Modeline:2 ends here
 
+;; [[file:config.org::*Which-Key][Which-Key:2]]
 (defun wjb/posframe-arghandler (buffer-or-name arg-name value)
   (let ((info '(:internal-border-width 2 :width 500 :height 48)))
     (or (plist-get info arg-name) value)))
 (setq posframe-arghandler #'wjb/posframe-arghandler)
+;; Which-Key:2 ends here
 
+;; [[file:config.org::*Keybindings][Keybindings:1]]
 ;; (define-key ivy-minibuffer-map (kbd "TAB") 'ivy-partial)
 ;; (define-key ivy-minibuffer-map (kbd "<return>") 'ivy-alt-done)
+;; Keybindings:1 ends here
 
+;; [[file:config.org::*All-The-Icons Ivy Rich][All-The-Icons Ivy Rich:2]]
 (use-package! all-the-icons-ivy-rich
   :after counsel-projectile
   :init (all-the-icons-ivy-rich-mode +1)
   :config
   (setq all-the-icons-ivy-rich-icon-size 0.8))
+;; All-The-Icons Ivy Rich:2 ends here
 
+;; [[file:config.org::*Ivy-Postframe][Ivy-Postframe:1]]
 (setq ivy-posframe-width 80)
+;; Ivy-Postframe:1 ends here
 
+;; [[file:config.org::*Vertico][Vertico:1]]
 (defun minibuffer-format-candidate (orig cand prefix suffix index _start)
   (let ((prefix (if (= vertico--index index)
                     "  " "   ")))
@@ -389,9 +550,16 @@ for what debugger to use. If the prefix ARG is set, prompt anyway."
 
 (advice-add #'vertico--format-candidate
             :around #'minibuffer-format-candidate)
+;; Vertico:1 ends here
 
+;; [[file:config.org::*Vertico][Vertico:2]]
 (setq vertico-count-format nil)
+;; Vertico:2 ends here
 
+;; [[file:config.org::*Vertico][Vertico:3]]
 (setq vertico-posframe-width 200)
+;; Vertico:3 ends here
 
+;; [[file:config.org::*Increase the amount of data which Emacs reads from the process][Increase the amount of data which Emacs reads from the process:1]]
 (setq read-process-output-max (* 1024 1024)) ;; 1mb
+;; Increase the amount of data which Emacs reads from the process:1 ends here
--- packages.el
+++ packages.el
@@ -1,45 +1,82 @@
+;; [[file:config.org::*Fix straight.el][Fix straight.el:1]]
 (package! straight :pin "3eca39d")
+;; Fix straight.el:1 ends here
 
+;; [[file:config.org::*Editorconfig][Editorconfig:1]]
 (package! editorconfig)
+;; Editorconfig:1 ends here
 
+;; [[file:config.org::*org-present][org-present:1]]
 (package! org-present)
+;; org-present:1 ends here
 
+;; [[file:config.org::*Add Org-Roam UI][Add Org-Roam UI:1]]
 (unpin! org-roam)
 (package! org-roam-ui)
 (package! websocket :pin "fda4455333309545c0787a79d73c19ddbeb57980") ; dependency of `org-roam-ui'
+;; Add Org-Roam UI:1 ends here
 
+;; [[file:config.org::*Google Calendar integration][Google Calendar integration:1]]
 (package! org-gcal)
+;; Google Calendar integration:1 ends here
 
+;; [[file:config.org::*Jest Test Mode][Jest Test Mode:1]]
 (package! jest-test-mode)
+;; Jest Test Mode:1 ends here
 
+;; [[file:config.org::*Svelte][Svelte:1]]
 (package! svelte-mode)
+;; Svelte:1 ends here
 
+;; [[file:config.org::*LSP][LSP:1]]
 (unpin! lsp-mode)
+;; LSP:1 ends here
 
+;; [[file:config.org::*Tailwind][Tailwind:1]]
 (package! lsp-tailwindcss
   :recipe (:host github
            :repo "merrickluo/lsp-tailwindcss"))
+;; Tailwind:1 ends here
 
+;; [[file:config.org::*Poetry][Poetry:1]]
 (package! poetry)
+;; Poetry:1 ends here
 
+;; [[file:config.org::*Load via nix somehow][Load via nix somehow:1]]
 (package! jsonrpc)
 (package! copilot)
 ;;   :recipe (:host github :repo "zerolfx/copilot.el" :files ("*.el" "dist")))
+;; Load via nix somehow:1 ends here
 
+;; [[file:config.org::*Doom Themes][Doom Themes:1]]
 (package! doom-themes)
+;; Doom Themes:1 ends here
 
+;; [[file:config.org::*Grayscale][Grayscale:1]]
 (package! grayscale-theme)
+;; Grayscale:1 ends here
 
+;; [[file:config.org::*Tao Themes][Tao Themes:1]]
 (package! tao-theme)
+;; Tao Themes:1 ends here
 
+;; [[file:config.org::*Nano Modeline][Nano Modeline:1]]
 (package! nano-modeline)
+;; Nano Modeline:1 ends here
 
+;; [[file:config.org::*Which-Key][Which-Key:1]]
 (package! which-key-posframe)
+;; Which-Key:1 ends here
 
+;; [[file:config.org::*All-The-Icons Ivy Rich][All-The-Icons Ivy Rich:1]]
 (package! all-the-icons-ivy-rich)
+;; All-The-Icons Ivy Rich:1 ends here
 
+;; [[file:config.org::*Treemacs-All-The-Icons][Treemacs-All-The-Icons:1]]
 ;; (package! treemacs-all-the-icons)
+;; Treemacs-All-The-Icons:1 ends here
 
+;; [[file:config.org::*Unsorted Packages][Unsorted Packages:1]]
 (package! dap-mode)
 
 ;; Orgmode
@@ -72,3 +109,4 @@
 ;; Multiple major modes in one buffer
 (package! polymode)
 (package! poly-markdown)
+;; Unsorted Packages:1 ends here
